2f5099607be31a768b2214fe47c3096af0a7f823
SunnySunhwa <via1229@gmail.com>
2017-11-30 16:04:09 +0900

    cleanning directories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
+  template: ` 
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center">
       <h1>
