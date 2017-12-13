c0ceba24283d307404ed957dbdb4cb718921c185
kw8799 <kw8799@gmail.com>
2017-12-13 20:46:02 +0900

    make layout of petstate

--- src/app/dev/pet-state/pet-state.component.ts
+++ src/app/dev/pet-state/pet-state.component.ts
@@ -8,32 +8,36 @@ import { Component, OnInit } from '@angular/core';
     <div class="flexbox">
       <ul class="flex-card-list">
         <!--### S: Card List ###-->
-        <li class="flex-card-listitem">
+        <li class="flex-card-listitem top-section">
           <div class="flex-card">
             현재 15kg, 목표 10kg
           </div>
+        </li>
+        <li class="flex-card-listitem top-section">
           <div class="flex-card">
             Normal Weight
           </div>
         </li>
       </ul>
       <ul class="flex-card-list">
-        <!--### S: Card List ###-->
-        <li class="flex-card-listitem">
+        <li class="flex-card-listitem mid-section">
           <div class="flex-card">
             그래프
           </div>
         </li>
       </ul>
       <ul class="flex-card-list">
-        <!--### S: Card List ###-->
-        <li class="flex-card-listitem">
+        <li class="flex-card-listitem bottom-section">
           <div class="flex-card">
             목 사이즈
           </div>
+        </li>
+        <li class="flex-card-listitem bottom-section">
           <div class="flex-card">
             가슴 사이즈
           </div>
+        </li>
+        <li class="flex-card-listitem bottom-section">
           <div class="flex-card">
             폼
           </div>
@@ -41,7 +45,7 @@ import { Component, OnInit } from '@angular/core';
       </ul>
     </div>
   `,
-  styles: []
+  styleUrls: ['./pet-state.scss']
 })
 export class PetStateComponent implements OnInit {
 
