af83e46652172229134b631fb3e06830d71a2486
kw8799 <kw8799@gmail.com>
2017-12-13 19:46:56 +0900

    add card styling

--- src/app/dev/pet-state/pet-state.component.ts
+++ src/app/dev/pet-state/pet-state.component.ts
@@ -4,20 +4,42 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-pet-state',
   template: `
     <h1>Pet State</h1>
-      <div class="container">
-        <div class="row">
-          <div class="">현재 15kg, 목표 10kg</div>
-          <div class="">Normal Weight</div>
-        </div>
-        <div class="row">
-          <div class="">그래프</div>
-        </div>
-        <div class="row">
-          <div class="">넥 사이즈</div>
-          <div class="">가슴 사이즈</div>
-          <div class="">인풋상자</div>
-        </div>
-      </div>
+    
+    <div class="flexbox">
+      <ul class="flex-card-list">
+        <!--### S: Card List ###-->
+        <li class="flex-card-listitem">
+          <div class="flex-card">
+            현재 15kg, 목표 10kg
+          </div>
+          <div class="flex-card">
+            Normal Weight
+          </div>
+        </li>
+      </ul>
+      <ul class="flex-card-list">
+        <!--### S: Card List ###-->
+        <li class="flex-card-listitem">
+          <div class="flex-card">
+            그래프
+          </div>
+        </li>
+      </ul>
+      <ul class="flex-card-list">
+        <!--### S: Card List ###-->
+        <li class="flex-card-listitem">
+          <div class="flex-card">
+            목 사이즈
+          </div>
+          <div class="flex-card">
+            가슴 사이즈
+          </div>
+          <div class="flex-card">
+            폼
+          </div>
+        </li>
+      </ul>
+    </div>
   `,
   styles: []
 })
