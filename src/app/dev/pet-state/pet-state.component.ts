97fef7779994dcfd2c8f40abf5491f8ac84cdba3
kw8799 <kw8799@gmail.com>
2017-12-13 19:35:45 +0900

    petstate markup

--- src/app/dev/pet-state/pet-state.component.ts
+++ src/app/dev/pet-state/pet-state.component.ts
@@ -3,9 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pet-state',
   template: `
-    <p>
-      pet-state works!
-    </p>
+    <h1>Pet State</h1>
+      <div class="container">
+        <div class="row">
+          <div class="">현재 15kg, 목표 10kg</div>
+          <div class="">Normal Weight</div>
+        </div>
+        <div class="row">
+          <div class="">그래프</div>
+        </div>
+        <div class="row">
+          <div class="">넥 사이즈</div>
+          <div class="">가슴 사이즈</div>
+          <div class="">인풋상자</div>
+        </div>
+      </div>
   `,
   styles: []
 })
